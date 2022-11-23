b66a6654af6a3a867a02866cddfea47b6362983c
Sriram823 <62587304+Sriram823@users.noreply.github.com>
2022-11-23 11:19:22 +0530

    Add files via upload

---
+++ src/main/java/com/java/Exam/App.java
@@ -0,0 +1,13 @@
+package com.java.Exam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab Internal 1 exam , the created project is Exam" );
+    }
+}
